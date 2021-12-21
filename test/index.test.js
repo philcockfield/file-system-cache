bd8a1536e56ade46124da9abf93a35fec765f466
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-22 05:28:40 +0900

    use 'new' keyword

--- test/index.test.js
+++ test/index.test.js
@@ -6,7 +6,7 @@ import FileSystemCache from "../src/cache";
 
 describe("Module entry API", function() {
   it("creates an instance of [FileSystemCache]", () => {
-    const cache = CacheFs();
+    const cache = new CacheFs();
     expect(cache).to.be.an.instanceof(FileSystemCache);
   });
 });
