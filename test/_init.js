57ebb4d3ea934322f98d12ecd31ab6babe452c96
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-22 05:28:34 +0900

    ignore ENOTEMPTY

--- test/_init.js
+++ test/_init.js
@@ -1,8 +1,16 @@
-import fs from "fs-extra";
-import fsPath from "path";
+import fs from 'fs-extra';
+import fsPath from 'path';
 
-const BASE_PATH = "./test/samples";
-const deleteFolder = () => fs.removeSync(fsPath.resolve(BASE_PATH));
+const BASE_PATH = './test/samples';
+const deleteFolder = () => {
+  try {
+    fs.emptyDirSync(fsPath.resolve(BASE_PATH));
+  } catch (e) {
+    if (!e.code === 'ENOTEMPTY') {
+      throw e;
+    }
+  }
+};
 
 beforeEach(() => deleteFolder());
 afterEach(() => deleteFolder());
