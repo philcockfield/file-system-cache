21572d901d5311e98409152aad2dda1e75ac40b0
Phil Cockfield <phil@cockfield.net>
2023-07-28 09:53:04 +1200

    rename param

--- src/common/util.crypto.ts
+++ src/common/util.crypto.ts
@@ -25,13 +25,13 @@ export function hashExists(algorithm: t.HashAlgorithm) {
  * Encrypt the given data.
  */
 export function encrypt(
-  secretKey: string,
+  secret: string,
   data: crypto.BinaryLike,
   options: { algorithm?: string; ivLength?: number } = {},
 ): t.EncryptedFile {
   const { algorithm = DEFAULTS.encrypt.algorithm, ivLength = DEFAULTS.encrypt.ivLength } = options;
   const iv = crypto.randomBytes(ivLength); // Initialization vector.
-  const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+  const cipher = crypto.createCipheriv(algorithm, secret, iv);
   const encrypted = Buffer.concat([cipher.update(data), cipher.final()]);
   return {
     iv,
@@ -44,7 +44,7 @@ export function encrypt(
  * Decrypt the given data
  */
 export function decrypt(
-  secretKey: string,
+  secret: string,
   input: string | t.EncryptedFile,
   options: { algorithm?: string } = {},
 ): t.DecryptedFile {
@@ -54,7 +54,7 @@ export function decrypt(
   const textParts = text.split(':');
   const iv = Buffer.from(textParts[0], 'hex');
   const encryptedText = Buffer.from(textParts[1], 'hex');
-  const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey), iv);
+  const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secret), iv);
   const decrypted = Buffer.concat([decipher.update(encryptedText), decipher.final()]);
 
   return {
