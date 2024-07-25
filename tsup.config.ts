0ddc1d43404206bb23e619742300dea07edd1ee4
Phil Cockfield <phil@cockfield.net>
2024-07-25 12:03:15 +1200

    variations on an ESM/CJS dual bundle configuration

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  minify: true,
+  outDir: 'lib',
+  treeshake: true,
+});
